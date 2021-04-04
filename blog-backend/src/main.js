62b8b62341418846a6fb9e44274d968472e00192
wondonghwi <ehdehd1289@naver.com>
2021-04-05 03:11:46 +0900

    feat : 정적파일 기능 제공

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -3,6 +3,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
@@ -11,7 +14,7 @@ import jwtMiddleware from './lib/jwtMiddleware';
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true })
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -23,7 +26,7 @@ const app = new Koa();
 const router = new Router();
 
 // 라우터 설정
-router.use('/api', api.routes()); // api 라우트 적용
+router.use('/api', api.routes());
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
@@ -31,6 +34,14 @@ app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 // PORT 가 지정되어있지 않다면 4000 을 사용
 const port = PORT || 4000;
